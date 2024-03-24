a97595ebf824a144f31bd755522e702198cb00c2
rjohnsen <rogercbjohnsen@gmail.com>
2024-03-24 08:58:03 +0100

    Adding function to write unparsed entry to a error file

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
     "io/ioutil"
     "os"
 	"github.com/akamensky/argparse"
+	"bufio"
+	"log"
+	"regexp"
 )
 
 /*
@@ -22,6 +25,7 @@ type ParserDirective struct {
 	XMLName		xml.Name	`xml:"parserdirective"`
 	Name		string		`xml:"name"`
 	Description	string		`xml:"description"`
+	Regex		string		`xml:"regex"`
 	Logfields	[]Logfield	`xml:"logfields>logfield"`
 }
 
@@ -46,8 +50,73 @@ func load_parser_directive(xml_path string) ParserDirective {
 }
 
 /*
-* Main application logic
-*/
+ * Parse log
+ */
+
+func parse_log(parser_directive ParserDirective, index_name *string, log_path *string) {
+	fmt.Printf("Index: %s\n", *index_name)
+	fmt.Printf("Parser Directive: %s\n", parser_directive.Name)
+	fmt.Printf("Log file: %s\n", *log_path)
+
+	file, err := os.Open(*log_path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	line_counter := 0
+
+	// Tracking errors
+	errors := 0
+	entries_indexed := 0
+
+	// Write lines with error to file for follow up
+	error_file, err := os.OpenFile("error.data", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Println(err)
+	}
+	
+	defer error_file.Close()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		line_counter += 1
+
+		re := regexp.MustCompile(parser_directive.Regex)
+		matches := re.FindStringSubmatch(line)
+
+		if len(matches) >= 8 {
+			fmt.Printf("%d - %d - %s => %s\n", line_counter, len(matches), matches[re.SubexpIndex("http_method")], matches[re.SubexpIndex("url")])
+			entries_indexed += 1
+		} else {
+			errors += 1
+			
+			if _, err := error_file.WriteString(fmt.Sprintf("%s\n", line)); err != nil {
+				log.Println(err)
+			}
+		
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nIndexing done. Here are som vital stats:")
+	fmt.Printf("Entries total: %d\n", line_counter)
+	fmt.Printf("Entries indexed: %d\n", entries_indexed)
+	fmt.Printf("Errors: %d\n", errors)
+
+}
+
+/*
+ * Main application logic
+ */
 
 func main() {
 	parser := argparse.NewParser("lapio", "Lapio - Log Shovel. Shovel logs into Elastic Search")
@@ -56,7 +125,7 @@ func main() {
 		Help: "Path to parser directive",
 	})
 
-	log_file_path := parser.String("l", "logpath", &argparse.Options{
+	log_path := parser.String("l", "logpath", &argparse.Options{
 		Required: true,
 		Help: "Path to log file",
 	})
@@ -72,9 +141,10 @@ func main() {
 		fmt.Print(parser.Usage(err))
 	} else {
 		var parser_directive = load_parser_directive(*parser_directive_path)
-
-		fmt.Printf("Index: %s", index_name)
-		fmt.Printf("Parser Directive: %s\n", parser_directive.Name)
-		fmt.Printf("Log file: %s\n", *log_file_path)
+		parse_log(
+			parser_directive,
+			index_name,
+			log_path,
+		)
 	}
 }
